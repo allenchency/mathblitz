9175dccf3add0386ffb28e47e4615d7f54faf0e1
Tuple <chen545788@gmail.com>
2024-08-05 15:40:37 +0800

    slide issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "35dfffc8fa590ad797f62fe155b7b77c",
+const RESOURCES = {"flutter_bootstrap.js": "0ad98307f3a0d2986a0c27b1b258f434",
 "version.json": "692322028ce5417083c8554c3ba3971e",
-"index.html": "30d6f1bb016592ff5009b4387475538d",
-"/": "30d6f1bb016592ff5009b4387475538d",
-"main.dart.js": "bb028040b64f74202ee7dc4cc1c2cc68",
+"index.html": "6bd9f6d73a8cacf758f1dfaece355cc8",
+"/": "6bd9f6d73a8cacf758f1dfaece355cc8",
+"main.dart.js": "c6d7a72fd9091bb75ec7aed913760427",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "e0487b8387d3f8dd76d5699c6e89d99b",
 "icons/Icon-192.png": "572b168c7b6d4acd411d5f4dd9e02393",
