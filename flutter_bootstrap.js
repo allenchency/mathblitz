f82850f3ef16f7b657fe47ca8c33e18b04bad2ae
Tuple <chen545788@gmail.com>
2024-08-29 13:16:42 +0800

    navigation bar animation update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1040617682"
+    serviceWorkerVersion: "3676560035"
   }
 });
